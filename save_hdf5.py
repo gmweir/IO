042099c8eec0f7175b4a139547ee183eea0fbc77
Gavin Weir <gavinmcweir@gmail.com>
2018-02-15 16:23:18 +0100

    added some data access stuff to pybaseutils ... this package needs to be nuked and separated into separate packages

--- save_hdf5.py
+++ save_hdf5.py
@@ -16,6 +16,7 @@ import os as _os
 
 #from scipy.io import savemat #,loadmat,whosmat
 from ..Struct import Struct
+#from pybaseutils.Struct import Struct
 
 __metaclass__ = type
 
@@ -32,28 +33,28 @@ class ReportInterface(object):
         if _os.path.exists(filename):
 #            raise ValueError('File %s exists, will not overwrite.' % filename)
             with _h5.File(filename, 'r+') as h5file:
-                cls.__recursively_save_dict_contents_to_group__(h5file, dic)            
+                cls.__recursively_save_dict_contents_to_group__(h5file, dic)
         else:
             with _h5.File(filename, 'w') as h5file:
                 cls.__recursively_save_dict_contents_to_group__(h5file, dic)
         #endif
 
     def __bytesit(item):
-        if item is '': 
+        if item is '':
             item = ' '
         # endif
         item = item.encode('ascii', 'xmlcharrefreplace')
 #        try:
 #            # fails with some unicode, strict utf-8
-#            item = item.encode('ascii')  # item.encode('utf-8')  
+#            item = item.encode('ascii')  # item.encode('utf-8')
 #        except UnicodeEncodeError:
 #            item = item.encode('utf-32')
 #        # endtry
         item = _np.string_(item)
 #                    # item = item.encode('ascii','ignore')
-#                    item = _np.string_(item)                   
+#                    item = _np.string_(item)
         return item
-        
+
     def __unbytesit(value):
         tmp = (value).decode("utf-8")
         ileft = tmp.find('&#')  # find first index from left
@@ -62,7 +63,7 @@ class ReportInterface(object):
             # Contains xml translated unicode text
             tmp = tmp[:ileft]+chr(int(tmp[ileft+2:iright]))+tmp[iright+1:]
             ileft = tmp.find('&#')  # find first index from left
-            iright = tmp.find(';')  # find first index from left                        
+            iright = tmp.find(';')  # find first index from left
         # end while
         return tmp
 
@@ -72,11 +73,11 @@ class ReportInterface(object):
             item = 'None'
 #       # Save string types
         if isinstance(item, (str,)):
-            item = cls.__bytesit(item)                                             
+            item = cls.__bytesit(item)
         if isinstance(item, (int,)):
-            item = _np.int64(item)                
+            item = _np.int64(item)
         if isinstance(item, (bool,)):
-            item = _np.uint8(item)                
+            item = _np.uint8(item)
         if isinstance(item, (float,)):
             item = _np.float64(item)
 
@@ -85,9 +86,9 @@ class ReportInterface(object):
             item = [cls.__fixlist(ii) for ii in item]
             item = _np.asarray(item)
             item = _np.atleast_1d(item)
-                
+
         return item
-        
+
     @classmethod
     def __recursively_save_dict_contents_to_group__(cls, h5file, dic):
         """..."""
@@ -109,56 +110,56 @@ class ReportInterface(object):
                 if isinstance(item, (bytes,)):
                     # save string types (byte-strings)
                     print(h5file, key, item)
-                    grp = (h5file[key] if key in h5file  
-                           else h5file.create_dataset(key, data=item, dtype=item.dtype)) 
-    
+                    grp = (h5file[key] if key in h5file
+                           else h5file.create_dataset(key, data=item, dtype=item.dtype))
+
                 # save dictionaries
                 elif isinstance(item, dict): # or isinstance(item.any(), dict):
                     print('Dictionary:', h5file, key)
-                    grp = h5file[key] if key in h5file else h5file.create_group(key)                                
+                    grp = h5file[key] if key in h5file else h5file.create_group(key)
                     cls.__recursively_save_dict_contents_to_group__(grp, item)
-    
+
                 # other types cannot be saved and will result in an error
                 elif isinstance(item, (Struct, )):
-                    print('Structure:', h5file, key)                
+                    print('Structure:', h5file, key)
     #                print('Skipping the user defined class with internal methods')
-                    grp = h5file[key] if key in h5file else h5file.create_group(key)                                
-                    cls.__recursively_save_dict_contents_to_group__( 
-                         grp, item.dict_from_class() )              
-    
-                elif isinstance(item, (_np.ndarray,)) and isinstance(_np.atleast_1d(item)[0], (dict,)):                                
-                   # item = _np.atleast_1d(item) 
+                    grp = h5file[key] if key in h5file else h5file.create_group(key)
+                    cls.__recursively_save_dict_contents_to_group__(
+                         grp, item.dict_from_class() )
+
+                elif isinstance(item, (_np.ndarray,)) and isinstance(_np.atleast_1d(item)[0], (dict,)):
+                   # item = _np.atleast_1d(item)
                    grp = h5file[key] if key in h5file else h5file.create_group(key)
                    #cls.__recursively_save_dict_contents_to_group__(grp, item.tolist())
                    for ii in range(len(item)):
                        keyii = key + "/list" + str(ii) + "/"
                        grp = h5file[keyii] if keyii in h5file else h5file.create_group(keyii)
-                       cls.__recursively_save_dict_contents_to_group__(grp, item[ii]) 
+                       cls.__recursively_save_dict_contents_to_group__(grp, item[ii])
                    # end for
-                  
+
                 elif isinstance(item, (_np.ScalarType, _np.ndarray)):
                 # elif isinstance(item, (_np.ScalarType,)):
                     # print(key, item, type(key), type(item))
                     print(h5file, key, item)
-                    
+
                     # If the key already exists in the file, delete it for overwriting
-                    if key in h5file: 
+                    if key in h5file:
                         del(h5file[key])
                     # end if
-                            
+
                     h5file.create_dataset(key, data=item, dtype=item.dtype)
-                        
+
                     try:
                         if not _np.all(h5file[key].value == item):
                             raise ValueError('The data representation in the HDF5 file does not match the original dict.')
                     except:
                         print('# == FAILURE == #:', h5file, key, item, h5file[key])
                     # endtry
-                    
+
                 elif item is None:
                     print(h5file, key, item)
-                    h5file[key] = _np.asarray([])                
-                    
+                    h5file[key] = _np.asarray([])
+
                 else:
                     print('Cannot save key: '+key)
                     raise ValueError('Cannot save %s type.' % type(item))
@@ -168,7 +169,7 @@ class ReportInterface(object):
     @classmethod
     def __load_dict_from_hdf5__(cls, filename, path=None):
         """..."""
-        if path is None: 
+        if path is None:
             path = '/'
         else:
             path = '/'+path
@@ -180,14 +181,14 @@ class ReportInterface(object):
     def __iteratively_load_dict_contents_from_list__(cls, h5file, path):
 
         ni = len(h5file[path])
-        ans = []       
+        ans = []
         for ii in range(ni):
             ans.append( cls.__recursively_load_dict_contents_from_group__(
                 h5file, path + '/' + 'list'+str(ii) + '/') )
         # end for
         return _np.asarray(ans)
-        
-                
+
+
     @classmethod
     def __recursively_load_dict_contents_from_group__(cls, h5file, path):
         """..."""
@@ -198,7 +199,7 @@ class ReportInterface(object):
                     # print(item.value)
                     if item.value == b'None':
                         ans[key] = None
-                    else:                    
+                    else:
                         ans[key] = cls.__unbytesit(item.value)
                     # endif
                 elif isinstance(_np.atleast_1d(item.value)[0], bytes):
@@ -210,14 +211,14 @@ class ReportInterface(object):
                             tmp.append(cls.__unbytesit(ii))
                         # end if
                     # end for
-                    ans[key] = _np.asarray(tmp)                    
+                    ans[key] = _np.asarray(tmp)
                 else:
                     ans[key] = item.value
                 # endif
             elif len(key)>3 and key[0:4] == 'list':
                 ans = cls.__iteratively_load_dict_contents_from_list__(
-                                h5file, path) 
-                                
+                                h5file, path)
+
             elif isinstance(item, _h5._hl.group.Group):
                 ans[key] = cls.__recursively_load_dict_contents_from_group__(
                                 h5file, path + '/' + key + '/')
@@ -227,24 +228,24 @@ class ReportInterface(object):
 
 def loadHDF5data(sfilename, path=None, sepfield=False, verbose=True):
 
-    HDF5data = ReportInterface.__load_dict_from_hdf5__(sfilename, path)    
+    HDF5data = ReportInterface.__load_dict_from_hdf5__(sfilename, path)
 
     if verbose:
-        print('### Successfully loaded dictionary from HDF5 file ###')              
-        print('### From name '+sfilename+' ###')                         
+        print('### Successfully loaded dictionary from HDF5 file ###')
+        print('### From name '+sfilename+' ###')
     # endif
-        
+
     if sepfield:
         HDF5data = [HDF5data[keys] for keys in HDF5data]
         HDF5data = tuple(HDF5data)
     # endif
-        
+
     return HDF5data
 #end def load_HDF5
-    
-    
+
+
 # -------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------- #    
+# -------------------------------------------------------------------------- #
 
 
 def test():
@@ -272,28 +273,28 @@ def test():
     if _os.path.exists(filename):
         _os.remove(filename)
     # endif
-       
+
     ReportInterface.__save_dict_to_hdf5__(ex, filename)
 
     # Load and test
     loaded = ReportInterface.__load_dict_from_hdf5__(filename)
     print('loaded using built-in function')
     _np.testing.assert_equal(loaded, ex)
-    print('check 1 passed!')    
+    print('check 1 passed!')
 
 
     loaded = loadHDF5data(filename, sepfield=False, verbose=True)
     print('loaded using wrapper function')
     _np.testing.assert_equal(loaded, ex)
-    print('check 2 passed!')    
-    
+    print('check 2 passed!')
+
     return ex, loaded
-    
-    
+
+
 if __name__ == "__main__":
     ex, loaded = test()
 # endif
-    
+
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
-    
+
