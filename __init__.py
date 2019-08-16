73788f6e193223ebfaa1abbf3069037b128207d3
Gavin Weir <gawe@ipp-hgw.mpg.de>
2019-08-16 21:36:58 +0200

    adding text utilities for fscanf, sscanf, etc

--- __init__.py
+++ __init__.py
@@ -4,23 +4,24 @@ Created on Wed May  4 15:11:07 2016
 
 @author: gawe
 """
-# ========================================================================== #    
+# ========================================================================== #
 
 from __future__ import absolute_import, with_statement, absolute_import, \
                        division, print_function, unicode_literals
 
-# ========================================================================== #    
-# ========================================================================== #    
+# ========================================================================== #
+# ========================================================================== #
 
 __version__ = "2018.05.24.17"
-__all__ = ['egDataFormatIO','save_hdf5', 'fdf'] #, 'save_txt']
+__all__ = ['egDataFormatIO','save_hdf5', 'fdf', 'txtutils'] #, 'save_txt']
 
-from . import egDataFormatIO, save_hdf5, fdf #save_txt  # analysis:ignore
+from . import egDataFormatIO, save_hdf5, fdf , txtutils #save_txt  # analysis:ignore
 
 from .save_hdf5 import ReportInterface as saveHDF5  # analysis:ignore
 from .save_hdf5 import loadHDF5data as loadHDF5  # analysis:ignore
 from .egDataFormatIO import egDataFormatIO as eg  # analysis:ignore
 from .fdf import Fdf as fdf # analysis:ignore
+from .txtutils import scanf, sscanf, fscanf, ftell, fseek, frewind, fgets, fgetl, findstr  # analysis:ignore
 
 # ===================================================================== #
 # ===================================================================== #
--- txtutils/base.py
+++ txtutils/base.py
@@ -4,6 +4,20 @@ Created on Tue Jul 16 21:36:05 2019
 
 @author: gawe
 """
+# ===================================================================== #
+# ===================================================================== #
+
+
+from __future__ import absolute_import, with_statement, absolute_import, \
+                       division, print_function, unicode_literals
+import sys
+
+__metaclass__ = type
+
+
+# ===================================================================== #
+# ===================================================================== #
+
 
 def ftell(fid):
     """
@@ -11,6 +25,7 @@ def ftell(fid):
             returns the current file position
     """
     return fid.tell()
+
 
 def fseek(fid, offset, origin='currentposition'):
     """
@@ -37,8 +52,10 @@ def fseek(fid, offset, origin='currentposition'):
     # end if
     return fid.seek(offset, origin)
 
+
 def frewind(fid):
     return fseek(fid, 0, 'start-of-file')
+
 
 def fgets(fid):
     """
@@ -46,6 +63,7 @@ def fgets(fid):
             returns the next line of a file while including the new line character
     """
     return fid.readline()
+
 
 def fgetl(fid):
     """
@@ -55,6 +73,7 @@ def fgetl(fid):
     return fgets(fid).replace('\n','')
 
 # ===================================================================== #
+
 
 def findstr(a_str, sub):
     start = 0
@@ -66,3 +85,161 @@ def findstr(a_str, sub):
 
 # ===================================================================== #
 # ===================================================================== #
+
+
+def getv(s=''):
+    """
+    Read 1 number/line using eval() from <stdin> or 'file' if specified.
+    If the first non-whitespace is not valid number characters
+    '(+-.0123456789', then the line will be skipped.
+    """
+    try:
+        if s == '':
+            fid = sys.stdin
+        else:
+            fid = open(s, 'r')
+        # end if
+
+        x = []
+        for a in fid:
+            a = a.strip()
+            if a != '' and a[0] in '(+-.0123456789':
+                x.append(eval(a))
+            # end if
+        # end for
+    except:
+        raise
+    finally:
+        try: fid.close()
+        except: pass
+    # end try
+    return x
+# end def getv
+
+
+def getv2(s=''):
+    """
+    Read 2 numbers/line using eval() from <stdin> or 'file' if specified.
+    If the first non-whitespace is not valid number characters
+    '(+-.0123456789', then the line will be skipped.
+    """
+    try:
+        if s == '':
+            fid = sys.stdin
+        else:
+            fid = open(s, 'r')
+        # end if
+        x, y = [], []
+        for a in fid:
+            a = a.strip()
+            if a != '' and a[0] in '(+-.0123456789':
+                b = a.split()
+                x.append(eval(b[0]))
+                y.append(eval(b[1]))
+            # end if
+        # end for
+    except:
+        raise
+    finally:
+        try: fid.close()
+        except: pass
+    # end try
+    return x, y
+# end def getv2
+
+# ========================================================================== #
+
+
+def printv(x, s=''):
+    """
+    Write 1 number/line to <stdout> or 'file' if specified.
+    """
+    out = ''
+    for a in x:
+        out += repr(a) + '\n'
+    if s == '':
+        sys.stdout.write(out)
+    else:
+        try:
+            fid = open(s, 'w')
+            fid.write(out)
+        except:
+            raise
+        finally:
+            fid.close()
+        # end try
+    # end if
+
+# end def printv
+
+
+# ========================================================================== #
+# ========================================================================== #
+
+
+#class txtdata(_pyut.Struct):
+#    """
+#    Class for saving data to a text file
+#    - sfilname - str - File name (one txt file)
+#            OR - list - List of file names (multiple txt files)
+#    - datain   - object - data structure
+#            OR - list - List of data structures (multiple txt files)
+#    """
+#    def __init__(self, sfilname=None, datain=None):
+#        self.sfilname = sfilname
+#        self.data = datain
+#        if (sfilname is not None) and (datain is not None):
+#            self.__runsav__()
+#        # endif
+#    # end def __init__
+#
+#    def __runsav__(self):
+#        if isinstance(sfilname, list):
+#            self.nfils = len(sfilname)
+#            self.savmult(sfilname, datain)
+#        else:
+#            self.nfils = 1
+#            self.savall(sfilname, datain)
+#        # endif
+#    # end def __runsav__
+#
+##    def mkfil(self, sfil):
+##        """
+##        Make the text file that will hold data
+##        input:
+##            sfil - str - text file name
+##        output:
+##            hfil - handle - Handle to open text file
+##        """
+##
+##        return hfil
+#
+#
+#    def savall(self, sfil, dat):
+#
+#        _np.savetxt(sfil, dat)
+#        return 1
+#    # enddef openclose
+#
+#    if isinstance(var, str):
+#        fmt = '%s'
+#    elif isinstance(var, float):
+#        fmt = '%f'
+#    elif isinstance(var, int):
+#        fmt = '%i'
+#    elif isinstance(var, )
+## end class txtdata()
+
+
+
+# ========================================================================== #
+# ========================================================================== #
+
+
+
+
+
+
+
+# ========================================================================== #
+# ========================================================================== #
