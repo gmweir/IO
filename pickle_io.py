d03cbaedac4d03dc903188c1db5c34386ec04f14
Gavin Weir <gawe@ipp-hgw.mpg.de>
2022-05-12 18:53:22 +0200

    using the hdf5_io pickle methods where possible

--- pickle_io.py
+++ pickle_io.py
@@ -5,14 +5,113 @@ Created on Thu May 12 11:17:25 2022
 @author: gawe
 """
 
-import cPickle
+
 import traceback
+import sys
 
 try:
     from IO.utils import Struct
 except:
     from .utils import Struct
 # end try
+
+
+if sys.version_info < (3,0):
+    import cPickle as pickle
+else:
+    import pickle
+# end if
+
+try:
+    # the hdf5_io module has a nice function that works with pickle files
+    try:
+        from IO.hdf5_io import save, load
+    except ImportError:
+        from .hdf5_io import save, load
+    # end try
+except ImportError:
+    # No h5py ... maybe
+    def save(data, filename, mode='w'):
+        """Save `data` to file with given `filename`.
+
+        This function guesses the type of the file from the filename ending.
+        Supported endings:
+
+        ============ ===============================
+        ending       description
+        ============ ===============================
+        .txt, .dat    ASCII without compression
+        ------------ -------------------------------
+        .pkl, .pickle Pickle without compression
+        ------------ -------------------------------
+        .pklz        Pickle with gzip compression.
+        ============ ===============================
+
+        Parameters
+        ----------
+        filename : str
+            The name of the file where to save the data.
+        mode : str
+            File mode for opening the file. ``'w'`` for write (discard existing file),
+            ``'a'`` for append (add data to exisiting file).
+            See :py:func:`open` for more details.
+        """
+        if filename.endswith('.txt') or filename.endswidth('.dat'):
+            with open(filename, mode=mode) as file:
+                # pickle.dump(data, file))
+                file.write(pickle.dumps(data))
+
+        elif filename.endswith('.pkl') or filename.endswith('.pickle'):
+            with open(filename, mode + 'b') as file:
+                pickle.dump(data, file)
+
+        elif filename.endswith('.pklz'):
+            import gzip
+            with gzip.open(filename, mode + 'b') as file:
+                pickle.dump(data, file)
+
+        else:
+            raise ValueError("Don't recognise file ending of " + repr(filename))
+        # end if
+    # end def save
+
+
+    def load(filename):
+        """Load data from file with given `filename`.
+
+        Guess the type of the file from the filename ending, see :func:`save` for possible endings.
+
+        Parameters
+        ----------
+        filename : str
+            The name of the file to load.
+
+        Returns
+        -------
+        data : obj
+            The object loaded from the file.
+        """
+        filename = str(filename)
+        if filename.endswith('.txt') or filename.endswith('.dat'):
+            with open(filename, 'r') as file:
+                data = pickle.loads(file.read())
+
+        elif filename.endswith('.pkl') or filename.endswith('.pickle'):
+            with open(filename, 'rb') as file:
+                data = pickle.load(file)
+
+        elif filename.endswith('.pklz'):
+            import gzip
+            with gzip.open(filename, 'rb') as file:
+                data = pickle.load(file)
+
+        else:
+            raise ValueError("Don't recognise file ending of " + repr(filename))
+        return data
+# end try
+
+# ========================== #
+# ========================== #
 
 
 class pickle_io(Struct):
@@ -39,10 +138,10 @@ class pickle_io(Struct):
 
         try:
             with open(filename, mode=mode) as file:
-                file.write(cPickle.dumps(self.__dict__))
+                file.write(pickle.dumps(self.__dict__))
         except:
             file = open(filename, mode=mode)
-            file.write(cPickle.dumps(self.__dict__))
+            file.write(pickle.dumps(self.__dict__))
             file.close()
         finally:
             try: file.close()
@@ -80,7 +179,7 @@ class pickle_io(Struct):
             try: file.close()
             except: pass
         # end try
-        self.__dict__ = cPickle.loads(dataPickle)
+        self.__dict__ = pickle.loads(dataPickle)
     # end def load
 # end class
 
