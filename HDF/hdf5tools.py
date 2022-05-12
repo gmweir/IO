377f144f9d8671aadc7af0cf2bb11c1a7c64031b
Gavin Weir <gawe@ipp-hgw.mpg.de>
2022-05-12 11:33:11 +0200

    rewrote the base of this strange little utility to be more safe for opening and closing

--- HDF/hdf5tools.py
+++ HDF/hdf5tools.py
@@ -1,33 +1,55 @@
-# hdf5tools holds useful tools for getting eigsolver (and other) data
-# into the hdf5 format for storage or importing into other analysis packages
+
+
+from __future_ import with_statement
 
 import h5py
 
-def WriteToH5(filename, variable_dict):
-	# function to write all variables in the dictionary to an hdf5 file
-        # given a dictionary of variable names and the variables,
-        # the variables will be stored in the given HDF5 file
-        #
-        # usage example:
-        #
-        ## WriteToH5(fname+'.hdf5', {'gammas':gammas,
-        ##                   'omegas':omegas,
-        ##                   'eigenmodes':eigenmodes,
-        ##                   'locations':locations,
-        ##                   'm_nums':m_nums,
-        ##                   'bfields':bfields})
+
+def WriteToH5(filename, variable_dict, verbose=True):
+    """
+    function to write all variables in the dictionary to an hdf5 file
+    given a dictionary of variable names and the variables,
+    the variables will be stored in the given HDF5 file
+
+    usage example:
+
+        WriteToH5(fname+'.hdf5', {'gammas':gammas,
+                          'omegas':omegas,
+                          'eigenmodes':eigenmodes,
+                          'locations':locations,
+                          'm_nums':m_nums,
+                          'bfields':bfields})
+    """
+
+    if filename[-5:] != '.hdf5':
+        filename=filename+'.hdf5'
+    # end if
 
 
-	if filename[-5:] != '.hdf5':
-		filename=filename+'.hdf5'
+    def code_to_execute():
+        for var_name in var_names:
+            dataset=h5f.create_dataset(var_name, data=variable_dict[var_name])
+        # end for
+        dt= h5py.special_dtype(vlen=str)
+        dataset=h5f.create_dataset('var_names', data=var_names, dtype=dt)
+    # end def
 
-	h5f=h5py.File(filename, 'w')
-
-	var_names=variable_dict.keys()
-	for var_name in var_names:
-		dataset=h5f.create_dataset(var_name, data=variable_dict[var_name])
-
-	dt= h5py.special_dtype(vlen=str)
-	dataset=h5f.create_dataset('var_names', data=var_names, dtype=dt)
-
-	h5f.close()
+    var_names=variable_dict.keys()
+    # if 1:
+    try:
+        with h5py.File(filename, 'w') as h5f:
+            code_to_execute()
+        # end with open
+    # else:
+    except IOError:
+        if verbose:
+            print('The file %s does not appear to exist or is not accessible')
+        # end if
+        raise
+    except:
+        h5f = h5py.File(filename, 'w')
+        code_to_execute()
+    finally:
+        try: h5f.close()
+        except: pass
+    # end try
