127fad5c862d0d971418a531dbfbb362f0e4b2ea
Gavin Weir <gawe@ipp-hgw.mpg.de>
2022-05-12 11:33:55 +0200

    reorganizing the layout of this package to be more logical. In the mean time I need to keep backwards compatiblity in the __init__

--- __init__.py
+++ __init__.py
@@ -13,15 +13,27 @@ from __future__ import absolute_import, with_statement, absolute_import, \
 # ========================================================================== #
 
 __version__ = "2018.05.24.17"
-__all__ = ['egDataFormatIO','save_hdf5', 'fdf', 'txtutils'] #, 'save_txt']
+__all__ = ['egDataFormatIO','HDF', 'fdf', 'txtutils'] #, 'save_txt']
 
-from . import egDataFormatIO, save_hdf5, fdf , txtutils #save_txt  # analysis:ignore
+from . import egDataFormatIO as lhd_io # analysis:ignore
+from . import HDF as hdf5_io      # analysis:ignore
+from . import fdf as fdf_io       # analysis:ignore
+from . import txtutils as txt_io  # analysis:ignore
 
-from .save_hdf5 import ReportInterface as saveHDF5  # analysis:ignore
-from .save_hdf5 import loadHDF5data as loadHDF5  # analysis:ignore
+# ======== #
+
+#  Backwards compatibility  # annoyingly filling the namespace
+#  -->  Needs to be cleaned up
+from . import egDataFormatIO, HDF, fdf , txtutils #save_txt  # analysis:ignore
+from .HDF import save_hdf5   # backwards compatibility
+from .HDF.save_hdf5 import ReportInterface as saveHDF5  # analysis:ignore
+from .HDF.save_hdf5 import loadHDF5data as loadHDF5  # analysis:ignore
 from .egDataFormatIO import egDataFormatIO as eg  # analysis:ignore
 from .fdf import Fdf as fdf # analysis:ignore
 from .txtutils import scanf, sscanf, fscanf, ftell, fseek, frewind, fgets, fgetl, findstr  # analysis:ignore
+
+# ======== #
+
 
 # ===================================================================== #
 # ===================================================================== #
